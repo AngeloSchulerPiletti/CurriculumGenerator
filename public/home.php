9287dbbbca7c760e94b2bca3d0f1cff58cfa0208
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-05-10 17:50:28 -0300

    updates at all

--- public/home.php
+++ public/home.php
@@ -14,45 +14,47 @@
         <img src="/favicon/employee.svg" width="100px">
         <h1>CURRICULUM <sub>GENERATOR</sub></h1>
     </header>
-
-    <form>
+    <main>
+    <form method="POST" action="/App/Controller/dataResolve.php">
         <section class="section_data" id="sec_1">
-            <h1>Escolha uma linda foto!</h1>
-            <input type="file" id="picture">
+            <h3>Escolha uma linda foto!</h3>
+            <input type="file" id="picture" name="picture">
         </section>
 
         <section class="section_data" id="sec_2">
-            <h1>Informe suas informações básicas</h1>
-            <input type="text" name="name" placeholder="Angelo Schuler Piletti">
-            <input type="text" name="from" placeholder="Esteio, Rio Grande do Sul">
-            <input type="date" name="birth" placeholder="13/12/2003">
+            <h3>Informe suas informações básicas</h3>
+            <input type="text"   name="name" placeholder="Angelo Schuler Piletti">
+            <input type="text"   name="from" placeholder="Esteio, Rio Grande do Sul">
+            <input type="date"   name="birth" placeholder="13/12/2003">
             <input type="number" name="phone" placeholder="+55 51 9XXXX-XXXX">
-            <input type="email" name="email" placeholder="angeloiletti@gmail.com">
-            <input type="text" name="link" placeholder="https://github.com/AngeloSchulerPiletti">
+            <input type="email"  name="email" placeholder="angeloiletti@gmail.com">
+            <input type="text"   name="link" placeholder="https://github.com/AngeloSchulerPiletti">
         </section>
         
         <section class="section_data" id="sec_3">
-            <h1>Seção 1</h1>
-            <input type="text" placeholder="Título">
-            <textarea placeholder="Diga algo incrível!"></textarea>
+            <h3>Seção 1</h3>
+            <input type="text" placeholder="Título" name="title1">
+            <textarea placeholder="Diga algo incrível!" name="text1"></textarea>
         </section>
         
         <section class="section_data" id="sec_4">
-            <h1>Seção 2</h1>
-            <input type="text" placeholder="Título">
-            <textarea placeholder="Diga a eles como você é bom!"></textarea>
+            <h3>Seção 2</h3>
+            <input type="text" placeholder="Título" name="title2">
+            <textarea placeholder="Diga a eles como você é bom!" name="text2"></textarea>
         </section>
         
         <section class="section_data" id="sec_5">
-            <h1>Seção 3</h1>
-            <input type="text" placeholder="Título">
-            <textarea placeholder="Talvez aquela experiência passada?"></textarea>
+            <h3>Seção 3</h3>
+            <input type="text" placeholder="Título" name="title3">
+            <textarea placeholder="Talvez aquela experiência passada?" name="text3"></textarea>
         </section>
 
         <section id="politic">
-            <input type="checkbox"><legend>Concordo com a política de privacidade!</legend>
+            <input type="checkbox" name="politicAgreement"><legend>Concordo com a política de privacidade!</legend>
             <button type="submit">ENVIAR</button>
         </section>
     </form>
+    </main>
+    <footer></footer>
 </body>
 </html>
