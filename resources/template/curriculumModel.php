83215540b6a226b8e41db6278bd67507431b1e85
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-05-11 21:10:49 -0300

    Form's values passed successfully to the curriculumModel.php and a HTML was made with no problems

--- resources/template/curriculumModel.php
+++ resources/template/curriculumModel.php
@@ -1,29 +1,55 @@
-<?php 
-
+<?php
+$inputs = [
+    "picture", "name", "from", "birth", "phone", "email", "link", "title1",
+    "text1", "title2", "text2", "title3", "text3"
+];
 ?>
-<header>
-    CURRICULUM MODEL
+
+<section id="curriculo">
+    <header>
+        CURRICULUM MODEL
         <img>
         <div>
-            <h6></h6>
-            <h6></h6>
-            <h6></h6>
-            <h6></h6>
-            <h6></h6>
-            <h6></h6>
+            <?php
+            $inputs = [
+                "picture", "name", "from", "birth", "phone", "email", "link",
+            ];
+
+            foreach ($_POST as $key => $value) {
+                if ($value != "") {
+                    foreach ($inputs as $field) {
+                        if ($key == $field) {
+                            echo "<h6>" . $value . "</h6>";
+                        }
+                    }
+                }
+            }
+            ?>
+
         </div>
-</header>
+    </header>
     <article>
-        <section>
-            <h3></h3>
-            <p></p>
-        </section>
-        <section>
-            <h3></h3>
-            <p></p>
-        </section>
-        <section>
-            <h3></h3>
-            <p></p>
-        </section>
+        <?php
+        $inputs = [
+            "title1", "text1", "title2", "text2", "title3", "text3"
+        ];
+
+        foreach ($_POST as $key => $value) {
+            if ($value != "") {
+                echo "<section>";
+                foreach ($inputs as $field) {
+                    if ($key == $field) {
+                        if (str_contains($key, "title")) {
+                            echo "<h3>" . $value . "</h3>";
+                        } else if (str_contains($key, "text")) {
+                            echo "<p>" . $value . "</p>";
+                        }
+                    }
+                }
+                echo "</section>";
+            }
+        }
+
+        ?>
     </article>
+</section>
